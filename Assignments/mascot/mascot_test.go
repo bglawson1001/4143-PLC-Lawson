b16a46501ed49899db5b22b70fc154d8693910ad
bglawson1001 <122930732+bglawson1001@users.noreply.github.com>
2023-09-07 10:34:50 -0500

    Create mascot_test.go

---
+++ Assignments/mascot/mascot_test.go
@@ -0,0 +1,15 @@
+package mascot_test
+
+import (
+	"testing"
+
+	"example.com/p01/mascot"
+)
+
+func TestMascot(t *testing.T) {
+	if mascot.BestMascot() != "Go Gopher" {
+
+		t.Fatal("Wrong mascot :(")
+	}
+
+}
