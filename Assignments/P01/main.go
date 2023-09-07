ebe0e58140d36fac2c9548e9a2a41c3b60665f90
bglawson1001 <122930732+bglawson1001@users.noreply.github.com>
2023-09-07 10:00:27 -0500

    Update main.go

--- Assignments/P01/main.go
+++ Assignments/P01/main.go
@@ -1,3 +1,28 @@
+/************************************************************************
+ *
+ *  Author:           Brayden Lawson
+ *  Title:            Run a Go program
+ *  Course:           4143-101
+ *  Semester:         Fall 2023
+ *
+ *  Description:
+ * Basic Golang program that checks to see what mascot is returned in a mascot.go file.
+ * Main.go prints out the name of the mascot along with a quote if the if statement in the
+*  mascot_test.go statement is not false.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *  Usage:
+ *        Shows how to import packages, write functions, and write a test file in Golang.
+ *
+ *
+ *  Files: mascot_test.go, mascot.go, main.go
+ ************************************************************************/
+
 package main
 
 import (
